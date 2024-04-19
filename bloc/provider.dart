f38ebe9edc314e979995c692ab6b9911ba583c55
Mdsadiq03 <mohamedsadiq2k3@gmail.com>
2024-04-19 21:03:35 +0530

    Feat: Initiated with bloc and provider

---
+++ bloc/provider.dart
@@ -0,0 +1,16 @@
+import 'package:food_delivery/model/fooditem.dart';
+
+class CartProvider{
+  
+  List<FoodItem> foodItems = [];
+  List<FoodItem> addToList(FoodItem foodItem){
+    foodItems.add(foodItem);
+    return foodItems;
+  }
+
+  List<FoodItem> removeFromList(FoodItem foodItem){
+    foodItems.remove(foodItem);
+    return foodItems;
+  }
+
+}
