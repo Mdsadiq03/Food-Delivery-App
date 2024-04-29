8adbabbb7df79739ab4e6c592130b4b109b97b91
Mdsadiq03 <mohamedsadiq2k3@gmail.com>
2024-04-29 19:13:02 +0530

    Feat:Added categories member function and class CategoriesListItem

--- main.dart
+++ main.dart
@@ -12,22 +12,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      blocs: [
-        Bloc((i) => CartListBloc())
-      ],
-      dependencies: const[],
-      child:const MaterialApp(
-      title: 'Food Delivery',
-      home: Home(),
-      debugShowCheckedModeBanner: false,
-    ),
+      blocs: [Bloc((i) => CartListBloc())],
+      dependencies: const [],
+      child: const MaterialApp(
+        title: 'Food Delivery',
+        home: Home(),
+        debugShowCheckedModeBanner: false,
+      ),
     );
   }
 }
 
-class Home extends StatelessWidget{
+class Home extends StatelessWidget {
   const Home({super.key});
-
 
   @override
   Widget build(BuildContext context) {
@@ -40,37 +37,50 @@ class Home extends StatelessWidget{
             ],
           ),
         ),
-        ),
-
+      ),
     );
-}
+  }
 }
 
-class FirstHalf extends StatelessWidget{
+class FirstHalf extends StatelessWidget {
   const FirstHalf({super.key});
-
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.fromLTRB(35, 25, 0, 0,),
+      padding: const EdgeInsets.fromLTRB(
+        35,
+        25,
+        0,
+        0,
+      ),
       child: Column(
         children: <Widget>[
           const CustomAppBar(),
-          const SizedBox(height: 30,),
+          const SizedBox(
+            height: 30,
+          ),
           title(),
-          const SizedBox(height: 30,),
+          const SizedBox(
+            height: 30,
+          ),
           searchBar(),
+          const SizedBox(
+            height: 45,
+          ),
+          categories(),
         ],
       ),
-      );
+    );
   }
 
-  Widget title(){
+  Widget title() {
     return const Row(
       mainAxisAlignment: MainAxisAlignment.start,
       children: <Widget>[
-        SizedBox(width: 45,),
+        SizedBox(
+          width: 45,
+        ),
         Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
@@ -91,19 +101,20 @@ class FirstHalf extends StatelessWidget{
           ],
         )
       ],
-
     );
   }
 
   Widget searchBar() {
     return const Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children:<Widget>[
+      children: <Widget>[
         Icon(
           Icons.search,
           color: Colors.black45,
         ),
-        SizedBox(width: 20,),
+        SizedBox(
+          width: 20,
+        ),
         Expanded(
           child: TextField(
             decoration: InputDecoration(
@@ -118,38 +129,97 @@ class FirstHalf extends StatelessWidget{
       ],
     );
   }
+
+  Widget categories() {
+    return Container(
+      height: 185,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: <Widget>[
+          CategoryListItem(
+            categoryIcon: Icons.bug_report,
+            categoryName: "Burgers",
+            availability: 12,
+            selected: true,
+          ),
+          CategoryListItem(
+            categoryIcon: Icons.bug_report,
+            categoryName: "Burgers",
+            availability: 12,
+            selected: false,
+          ),
+        ],
+      ),
+    );
+  }
 }
 
-class CustomAppBar extends StatelessWidget{
+class CustomAppBar extends StatelessWidget {
   const CustomAppBar({super.key});
-
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.only(bottom: 15),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children:<Widget>[
-          const Icon(Icons.menu),
-          GestureDetector(
-            onTap: (){},
-            child: Container(
-              margin: const EdgeInsets.only(right: 30),
-              padding: const EdgeInsets.all(15),
-              decoration: BoxDecoration(
-                color: Colors.yellow[800],
-                borderRadius: BorderRadius.circular(50),
-             
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            const Icon(Icons.menu),
+            GestureDetector(
+              onTap: () {},
+              child: Container(
+                margin: const EdgeInsets.only(right: 30),
+                padding: const EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                  color: Colors.yellow[800],
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                child: const Text('0'),
               ),
-              child: const Text('0'),
             ),
-          ),
-
-        
-      ]),
-
+          ]),
     );
   }
 }
 
+class CategoryListItem extends StatelessWidget {
+  const CategoryListItem({
+    super.key,
+    required this.categoryIcon,
+    required this.categoryName,
+    required this.availability,
+    required this.selected,
+  });
+
+  final IconData categoryIcon;
+  final String categoryName;
+  final int availability;
+  final bool selected;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(right: 20),
+      padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(50),
+        color: selected ? Color(0xfffeb324) : Colors.white,
+        border: Border.all(
+          color: selected ? Colors.transparent : Colors.grey,
+          width: 1.5,
+        ),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.grey,
+            blurRadius: 15,
+            offset: Offset(25, 0),
+            spreadRadius: 5,
+
+          ),
+        ]
+      ),
+    
+      );
+    
+  }
+}
