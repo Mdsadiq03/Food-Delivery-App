f0e4ee2f12927a1a440cd039ed2593adc02f8855
Mdsadiq03 <mohamedsadiq2k3@gmail.com>
2024-04-30 22:26:56 +0530

    Feat: Added more CatogoryListItems

--- main.dart
+++ main.dart
@@ -135,19 +135,37 @@ class FirstHalf extends StatelessWidget {
       height: 185,
       child: ListView(
         scrollDirection: Axis.horizontal,
-        children: <Widget>[
+        children: const <Widget>[
           CategoryListItem(
-            categoryIcon: Icons.bug_report,
-            categoryName: "Burgers",
-            availability: 12,
-            selected: true,
-          ),
-          CategoryListItem(
-            categoryIcon: Icons.bug_report,
-            categoryName: "Burgers",
-            availability: 12,
-            selected: false,
-          ),
+          categoryIcon: Icons.bug_report,
+          categoryName: "Burgers",
+          availability: 12,
+          selected: true,
+        ),
+        CategoryListItem(
+          categoryIcon: Icons.bug_report,
+          categoryName: "Pizza",
+          availability: 12,
+          selected: false,
+        ),
+        CategoryListItem(
+          categoryIcon: Icons.bug_report,
+          categoryName: "Rolls",
+          availability: 12,
+          selected: false,
+        ),
+        CategoryListItem(
+          categoryIcon: Icons.bug_report,
+          categoryName: "Burgers",
+          availability: 12,
+          selected: false,
+        ),
+        CategoryListItem(
+          categoryIcon: Icons.bug_report,
+          categoryName: "Burgers",
+          availability: 12,
+          selected: false,
+        ),
         ],
       ),
     );
