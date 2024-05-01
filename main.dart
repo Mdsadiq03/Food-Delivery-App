19dc09e64c741ab0e95fd675c70259a1617c74ff
Mdsadiq03 <mohamedsadiq2k3@gmail.com>
2024-05-01 11:39:23 +0530

    Feat: Added Classes Items and ItemContainer

--- main.dart
+++ main.dart
@@ -1,6 +1,7 @@
 import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
 import 'package:food_delivery/bloc/cartListBloc.dart';
+import 'package:food_delivery/model/fooditem.dart';
 
 void main() {
   runApp(const MyApp());
@@ -32,12 +33,103 @@ class Home extends StatelessWidget {
       body: SafeArea(
         child: Container(
           child: ListView(
-            children: const <Widget>[
+            children: <Widget>[
               FirstHalf(),
+              SizedBox(
+                height: 45,
+              ),
+              for (var foodItem in fooditemList.foodItems)
+                ItemContainer(foodItem: foodItem)
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class ItemContainer extends StatelessWidget {
+  const ItemContainer({super.key, required this.foodItem});
+
+  final FoodItem foodItem;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Items(
+        hotel: foodItem.hotel,
+        itemName: foodItem.title,
+        itemPrice: foodItem.price,
+        imgUrl: foodItem.imgUrl,
+        leftAligned: (foodItem.id % 2 == 0) ? true : false,
+      ),
+    );
+  }
+}
+
+class Items extends StatelessWidget {
+  const Items({
+    super.key,
+    required this.leftAligned,
+    required this.imgUrl,
+    required this.hotel,
+    required this.itemName,
+    required this.itemPrice,
+  });
+
+  final bool leftAligned;
+  final String imgUrl;
+  final String itemName;
+  final double itemPrice;
+  final String hotel;
+
+  @override
+  Widget build(BuildContext context) {
+    double containerPadding = 45;
+    double containerBorderRadius = 10;
+
+    return Column(
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.only(
+            left: leftAligned ? 0 : containerPadding,
+            right: leftAligned ? containerPadding : 0,
+          ),
+          child: Column(
+            children: <Widget>[
+              Container(
+                width: double.infinity,
+                height: 200,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.horizontal(
+                    left: leftAligned
+                        ? Radius.circular(0)
+                        : Radius.circular(containerBorderRadius),
+                    right: leftAligned
+                        ? Radius.circular(containerBorderRadius)
+                        : Radius.circular(0),
+                  ),
+                  child: Image.network(
+                    imgUrl,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+              SizedBox(height: 20,),
+              Container(
+                padding: EdgeInsets.only(
+                  left: leftAligned ? 20 : 0,
+                  right: leftAligned ? 0 : 20,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
@@ -137,35 +229,35 @@ class FirstHalf extends StatelessWidget {
         scrollDirection: Axis.horizontal,
         children: const <Widget>[
           CategoryListItem(
-          categoryIcon: Icons.bug_report,
-          categoryName: "Burgers",
-          availability: 12,
-          selected: true,
-        ),
-        CategoryListItem(
-          categoryIcon: Icons.bug_report,
-          categoryName: "Pizza",
-          availability: 12,
-          selected: false,
-        ),
-        CategoryListItem(
-          categoryIcon: Icons.bug_report,
-          categoryName: "Rolls",
-          availability: 12,
-          selected: false,
-        ),
-        CategoryListItem(
-          categoryIcon: Icons.bug_report,
-          categoryName: "Burgers",
-          availability: 12,
-          selected: false,
-        ),
-        CategoryListItem(
-          categoryIcon: Icons.bug_report,
-          categoryName: "Burgers",
-          availability: 12,
-          selected: false,
-        ),
+            categoryIcon: Icons.bug_report,
+            categoryName: "Burgers",
+            availability: 12,
+            selected: true,
+          ),
+          CategoryListItem(
+            categoryIcon: Icons.bug_report,
+            categoryName: "Pizza",
+            availability: 12,
+            selected: false,
+          ),
+          CategoryListItem(
+            categoryIcon: Icons.bug_report,
+            categoryName: "Rolls",
+            availability: 12,
+            selected: false,
+          ),
+          CategoryListItem(
+            categoryIcon: Icons.bug_report,
+            categoryName: "Burgers",
+            availability: 12,
+            selected: false,
+          ),
+          CategoryListItem(
+            categoryIcon: Icons.bug_report,
+            categoryName: "Burgers",
+            availability: 12,
+            selected: false,
+          ),
         ],
       ),
     );
@@ -220,67 +312,65 @@ class CategoryListItem extends StatelessWidget {
       margin: EdgeInsets.only(right: 20),
       padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(50),
-        color: selected ? Color(0xfffeb324) : Colors.white,
-        border: Border.all(
-          color: selected ? Colors.transparent : Colors.grey,
-          width: 1.5,
-        ),
-        boxShadow: const [
-          BoxShadow(
-            color: Colors.grey,
-            blurRadius: 15,
-            offset: Offset(25, 0),
-            spreadRadius: 5,
-
+          borderRadius: BorderRadius.circular(50),
+          color: selected ? Color(0xfffeb324) : Colors.white,
+          border: Border.all(
+            color: selected
+                ? Colors.transparent
+                : Color.fromARGB(44, 110, 109, 109),
+            width: 1.5,
           ),
-        ]
-      ),
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: <Widget>[
-        Container(
-          padding: EdgeInsets.all(20),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(20),
-            border: Border.all(
-              color: selected ? Colors.transparent : Colors.grey,
-              width: 1.5,
+          boxShadow: const [
+            BoxShadow(
+              color: Color.fromARGB(19, 167, 161, 161),
+              blurRadius: 15,
+              offset: Offset(25, 0),
+              spreadRadius: 5,
             ),
-            
+          ]),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20),
+              border: Border.all(
+                color: selected ? Colors.transparent : Colors.grey,
+                width: 1.5,
+              ),
+            ),
+            child: Icon(
+              categoryIcon,
+              color: Colors.black,
+              size: 30,
+            ),
           ),
-          child: Icon(
-            categoryIcon,
-            color: Colors.black,
-            size: 30,
+          SizedBox(
+            height: 10,
           ),
-        ),
-        SizedBox(height: 10,),
-        Text(
-          categoryName,
-          style: TextStyle(
-            fontWeight: FontWeight.w700,
-            color: Colors.black,
+          Text(
+            categoryName,
+            style: TextStyle(
+              fontWeight: FontWeight.w700,
+              color: Colors.black,
+            ),
           ),
-        ),
-        Container(
-          margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
-          width: 1.5,
-          height: 15,
-
-        ),
-        Text(
-          availability.toString(),
-          style: TextStyle(
-            fontWeight: FontWeight.w600,
-            color: Colors.black,
+          Container(
+            margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
+            width: 1.5,
+            height: 15,
           ),
-        ),
-        
-      ],
-    ),
-      );
-    
+          Text(
+            availability.toString(),
+            style: TextStyle(
+              fontWeight: FontWeight.w600,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
